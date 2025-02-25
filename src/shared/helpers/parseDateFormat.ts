b2dd7a8ccf1693ea2ca25f832f00fba0b8dbab2a
Pedro Victor <pedrols@seed.se.gov>
2025-02-25 11:43:57 -0300

    chore(helpers): cria diretório de funções helpers com a função de parsear data usando a lib date-fns

---
+++ src/shared/helpers/parseDateFormat.ts
@@ -0,0 +1,16 @@
+import { AppError } from "@shared/middlewares/errorHandler";
+import { isValid, parse } from "date-fns";
+
+export function parseDateFormat(dateStr: string): Date {
+    let parsedDate = parse(dateStr, "dd-MM-yyyy", new Date());
+
+    if (!isValid(parsedDate)) {
+        parsedDate = parse(dateStr, "dd/MM/yyyy", new Date());
+    }
+
+    if (!isValid(parsedDate)) {
+        throw new AppError("Invalid date format", 400);
+    }
+
+    return parsedDate;
+}
