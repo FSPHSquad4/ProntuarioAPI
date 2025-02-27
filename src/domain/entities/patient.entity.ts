2e17caffa18113f5f7d4dd3d7ec0c3647837fcb6
Pedro Victor <pedrols@seed.se.gov>
2025-02-27 13:19:36 -0300

    refactor(domain/entities): cria nova classe abstrata de Pessoa para que Paciente e Profissional herdem da mesma

--- src/domain/entities/patient.entity.ts
+++ src/domain/entities/patient.entity.ts
@@ -1,26 +1,14 @@
 import { Entity, Property } from "@mikro-orm/core";
-import { BaseEntity } from "./base.entity.ts";
+import { Person } from "./person.entity.ts";
 
 @Entity()
-export class Patient extends BaseEntity {
-    @Property()
-    fullName!: string;
-
+export class Patient extends Person {
     @Property({ unique: true, length: 11 })
     cpf!: string;
 
-    @Property({ type: "date" })
-    birthDate!: Date;
-
-    @Property({ length: 1, type: "char" })
-    gender!: string;
-
-    @Property({ length: 1, type: "char" })
-    maritalStatus!: string;
-
     @Property()
-    companionName!: string;
+    companionName?: string;
 
     @Property({ length: 11 })
-    companionCpf!: string;
+    companionCpf?: string;
 }
